a38450bf5d91d303ea4b12cbb025763404144921
Qwh <1491240752@qq.com>
2025-01-27 00:31:08 +0800

    初始化

---
+++ src/main/java/com/qwh/Main.java
@@ -0,0 +1,17 @@
+package com.qwh;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.qwh
+ * Description:
+ *
+ * @Author Qwh
+ * @Create 2025/1/27 0:22
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("测试代码评审组件");
+    }
+}
